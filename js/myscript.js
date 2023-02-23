4f0ab7edb9d96162332de85ddab76be45e144f27
Edward Zota <zotaedward@gmail.com>
2023-02-23 17:28:27 +0100

    esercizio concluso

--- js/myscript.js
+++ js/myscript.js
@@ -8,29 +8,68 @@ const age = document.querySelector('#age');
 
 const priceKm = 0.21 ;
 
+const reset = document.querySelector('#reset')
+
+const d_none_title = document.querySelector('.d-none-title')
+const d_none = document.querySelector('.d-none')
+
+/*parte del biglietto*/
+
+const nameTicket = document.querySelector('#name_on_ticket');
+
+const typeTicket = document.querySelector('#type_of_ticket');
+
+const wagonTicket = document.querySelector('#wagon_on_ticket');
+
+const codeTicket = document.querySelector('#code_on_ticket');
+
+const priceTicket = document.querySelector('#price_on_ticket');
+
+
+
+reset.addEventListener('click',
+    function eraseText() {
+        document.getElementById("name_surname").value = "";
+        document.getElementById("km").value = "";
+        
+        d_none_title.classList.remove('d-block');
+        d_none_title.classList.add('d-none');
+
+        d_none.classList.remove('d-block');
+        d_none.classList.add('d-none');
+}
+)
 
 
 domButton.addEventListener('click',
     function() {
-        const nameDom = myName;
-        const nameMessage = nameDom.value;
-        console.log(nameMessage);
+        nameTicket.innerHTML = myName.value;
 
-        const kmDom = km;
-        const kmMessage = kmDom.value;
-        console.log(kmMessage);
+        const kmMessage = km.value;
 
-        console.log(age.value);
-
-        let totalPrice = priceKm * km.value;
+        let totalPrice = priceKm * kmMessage;
+        typeTicket.innerHTML = 'Biglietto Standard';
 
         if  (age.value == "minorenne"){
             totalPrice -= totalPrice / 100 * 20;
+            typeTicket.innerHTML = 'Biglietto con sconto minorenne';
         }else if (age.value == "over65") {
             totalPrice = totalPrice - (totalPrice * 0.4);
+            typeTicket.innerHTML = 'Biglietto con sconto anziani';
         }
 
+        priceTicket.innerHTML = totalPrice.toFixed(2);
         console.log(totalPrice);
+
+        d_none_title.classList.remove('d-none');
+        d_none_title.classList.add('d-block');
+
+        d_none.classList.remove('d-none');
+        d_none.classList.add('d-block');
+
+        wagonTicket.innerHTML = Math.floor(Math.random() * 9 ) + 1;
+
+        codeTicket.innerHTML = Math.floor(Math.random() * 10000 ) + 90000;
 
     }
 )
